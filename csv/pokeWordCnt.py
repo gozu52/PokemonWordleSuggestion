811a5da393d152a1149c1e192ee37366c6c825bd
gozu52 <bknb0225@gmail.com>
2022-02-22 21:52:03 +0900

    updated contet to count each words

---
+++ csv/pokeWordCnt.py
@@ -0,0 +1,12 @@
+import pokeCsv
+
+ans = pokeCsv.ans
+cntWord=[0]*46
+
+for i in range(len(ans)):
+    for j in range(len(ans[i])):
+        if ans[i][j] >= 'ｰ' and ans[i][j] <= 'ﾝ':
+            cntTmp = ord(ans[i][j])-65392
+            cntWord[cntTmp] += 1
+print("cntWord","\n")
+print(cntWord)
